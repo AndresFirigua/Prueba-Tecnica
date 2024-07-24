9b9de6fac45adaa77bbaf2be3fce697fefa4df25
AndresFirigua <andresfirigua2003@gmail.com>
2024-07-24 10:44:30 -0500

    Ultimos cambios realizados, correcion en impresion

--- challengeOne/Main.java
+++ challengeOne/Main.java
@@ -1,6 +1,5 @@
 package challengeOne;
 
-import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
--- challengeTwo/Main.java
+++ challengeTwo/Main.java
@@ -1,16 +1,15 @@
 package challengeTwo;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int[] potencias = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 100000000};
-        int nCondicion = 66, nCantidad, n, cant = 0, cifras = 0, b = 0;
-        int auxiliar[] = new int[9];
+        int nCondicion=6 , nCantidad;
+        int nCondicion2=nCondicion*10+nCondicion;
+        boolean primero = true;
 
         // Entrada array en orden ascendente
+        System.out.println("condicion 2: "+nCondicion2);
         System.out.println("Ingrese cantidad de numeros del array: ");
         nCantidad = entrada.nextInt();
         int[] numeros = new int[nCantidad];
@@ -19,82 +18,36 @@ public class Main {
             numeros[i] = entrada.nextInt();
         }
 
-        //Elementos al cuadrado:
         int[] resultante = new int[nCantidad];
-        for (int i = 0; i < nCantidad; i++) {
+        for (int i = 0; i < nCantidad; i++) {   
             resultante[i] = numeros[i] * numeros[i];
+            if(resultante[i]>=nCondicion2){
+                resultante[i]=-1;
+            }
         }
 
-        // Eliminar de array resultante los números iguales o mayores de 66
-        for (int j = 0; j < nCantidad; j++) {
-            n = resultante[j];
-            cant = 0;
-            cifras = 0;
-
-            while (n != 0) {
-                n = n / 10;
-                cifras++;
-            }
-
-            for (int i = 0; i < 9; i++) { // Se obtiene cada uno de los valores de unidades, decenas, centenas, unidades de mil...
-                auxiliar[i] = (resultante[j] / potencias[i]) % 10;
-                if (resultante[j] >= nCondicion) {
-                    auxiliar[i] = -1;
-                    //System.out.println(auxiliar[i]);
-                    cant++;
-                }else if (resultante[j] * resultante[j] == 0) {
-                    auxiliar[i] = auxiliar[i];
-                    cant++;
-                }
-            }
-            resultante[j] = 0;
-            for (int i = 0; i < 9; i++) {
-                if (auxiliar[i] > -1) {
-                    resultante[j] = resultante[j] + auxiliar[i] * potencias[b];
-                    b++;
-                }
-            }
-            if (cifras == cant) {
-                resultante[j] = -1;
-            }
-            b = 0;
-        }
-
-        // Filtrar el array resultante
-        int validCount = 0;
+        //Ordenar resultante(Burbuja)
         for (int i = 0; i < nCantidad; i++) {
-            if (resultante[i] != -1) {
-                validCount++;
-            }
-        }
-
-        int[] filtrado = new int[validCount];
-        int index = 0;
-        for (int i = 0; i < nCantidad; i++) {
-            if (resultante[i] != -1) {
-                filtrado[index++] = resultante[i];
-            }
-        }
-
-        // Ordenar el array resultante (burbuja)
-        for (int i = 0; i < validCount - 1; i++) {
-            for (int j = 0; j < validCount - 1 - i; j++) {
-                if (filtrado[j] > filtrado[j + 1]) {
-                    int temp = filtrado[j];
-                    filtrado[j] = filtrado[j + 1];
-                    filtrado[j + 1] = temp;
+            for (int j = 0; j < nCantidad - i - 1; j++) {
+                if (resultante[j] > resultante[j + 1]) {
+                    int tmp = resultante[j + 1];
+                    resultante[j + 1] = resultante[j];
+                    resultante[j] = tmp;
                 }
             }
         }
-
-        // Mostrar el array resultante usando un ciclo for
+        
+        //Mostrado de resultado
         System.out.print("[");
-        for (int i = 0; i < filtrado.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
+        for(int i=0;i<nCantidad;i++){
+            if(resultante[i]!=-1){
+                if (!primero) {
+                    System.out.print(", ");
+                }
+                System.out.print(resultante[i]);
+                primero = false;
             }
-            System.out.print(filtrado[i]);
         }
-        System.out.println("]"); 
+        System.out.println("]");
     }
 }
